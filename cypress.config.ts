26d44e6420ab39df3096051c6332b48329ff2222
Vladislav Shmelev <vladislav787@rambler.ru>
2024-05-20 19:48:02 +0400

    minor fixes in config = TODO reporting

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,24 +4,24 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 let baseUrl="https://demoqa.com/";
 
 module.exports = defineConfig({
-  "reporter": "@shelex/cypress-allure-plugin/reporter",
-  "allure": {
-    "resultsDir": "allure-results",
-    "disableWebdriverStepsReporting": true,
-    "disableWebdriverScreenshotsReporting": true,
-    "allureTestOps": {
-      "enabled": true,
-     // "url": "https://allure.contrust.team/",
-     // "projectId": process.env.PROJECT_ID,
-     // "token": "glpat-nNUcenB9FfRyKepsHxw1"
-    }
-  },
+  // "reporter": "@shelex/cypress-allure-plugin/writer",
+  // "allure": {
+  //   "resultsDir": "allure-results",
+  //   "disableWebdriverStepsReporting": true,
+  //   "disableWebdriverScreenshotsReporting": true,
+  //   "allureTestOps": {
+  //     "enabled": false,
+  //    // "url": "https://allure.contrust.team/",
+  //    // "projectId": process.env.PROJECT_ID,
+  //    // "token": "glpat-nNUcenB9FfRyKepsHxw1"
+  //   }
+  // },
   retries: {
     runMode: 1,
     openMode: 0,
   },
   env: {    
-    hideXHR: true
+    hideXHR: true,
   },
   viewportWidth: 1920,
   viewportHeight: 1080,
@@ -35,7 +35,7 @@ module.exports = defineConfig({
     numTestsKeptInMemory: 10,
     video: false,
     setupNodeEvents(on, config) {
-      allureWriter(on, config);
+      //allureWriter(on, config);
       return config;
     },
   },
