c526c09b265ee8d9a76a1bdab0bb67d3081aafd9
Vladislav Shmelev <vladislav787@rambler.ru>
2024-06-05 18:32:33 +0400

    CI changes to parallel run

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -32,23 +32,6 @@ Cypress.on('test:after:run', (test, runnable) => {
 Cypress.on('uncaught:exception', (err, runnable) => {
   return false
 })
-//делаем скрин после каждого фейла теста и отправляем его в репорт
-// Cypress.on('test:after:run', (test, runnable) => {
-//     if (test.state === 'failed') {
-//         const screenshot = `${Cypress.config('screenshotsFolder')}\\${Cypress.spec.name
-//             }\\${runnable.parent.title} -- ${test.title} (failed)`;
-//        // cy.screenshot(`${Cypress.spec.name}\\${runnable.parent.title} -- ${test.title} (fail)`)
-//         cy.readFile(`${screenshot}.png`, 'base64')
-//             .then(file => {
-//                 //@ts-ignore
-//                 cy.error(`${test.title}: has failed`, {
-//                     name: `${runnable.parent.title} -- ${test.title} (failed).png`,
-//                     type: 'image/png',
-//                     content: file,
-//                 })
-//             })
-//     }
-// })
 
 // Hide fetch/XHR requests
 if (Cypress.env('hideXHR')) {
